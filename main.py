e966a0814e455516017175749949b0b40b309fcb
Myriam-Ramirez <myriam.ramirez.trejo@gmail.com>
2024-10-06 20:59:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
-#Welcome user: Implement the feature to welcome the user at the beginning of the conversation.
-def welcome_user():
-    print ("Welcome to the Pet Care Advisor Chatbot! My name is Py")
+def welcome_user(): 
+  print ("Welcome to the Pet Care Advisor Chatbot! My name is Py")
 
-#Collect the user's name and age
-def collect_users_information():
-    print("I cant wait to get to know you and your pet better!")
-    name= input("May I first have your name, please?")
-    print("Prrfect! Nice to meet you {name} I hope I can help you take care of your pet")
-    age= input("How old are you, {name}?")
-    return name, age
+def collect_user_info():
+  print ("I can't wait to get to know you and your pet better!")
+  name = input("What is your name? ")
+  age = input("How old are you? ")
+  print ("Prrfect! Nice to meet you, " + name+"!")
+  print ("I see you are " + age + " years old.")
+  return name, age
 
-#Collect user's dog information
-def collect_pet_information():
-    pet_name= input("What's your pet's name?")
-    pet_breed= input(f"What breed is {pet_name}?")
-    pet_age=input(f"How old is {pet_name}?")
-    return pet_name, pet_breed, pet_age
+def collect_pet_info():
+  pet_name = input("What's your pet's name? ")
+  pet_type = input("What type of animal is your pet? ")
+  pet_age = input("How old is your pet? ")
+  print ("I see your pet's name is " + pet_name + ", it is a " + pet_type +"")
+  return pet_name, pet_type 
 
-#Ask the user how it can help them
 def display_menu():
-    print ("\nHow can I assist you today?")
-    print("1. Set a feeding reminder")
-    print("2. Get health tips for your pet")
-    print("3. Get training advice")
-    print("4. Find pet-friendly places")
-    print("5. Exit")
+  print("How can I help you today?")
+  print("1. Set a feeding reminder")
+  print("2. Get health tips for your pet")
+  print("3. Get training advice")
+  print("4. Find pet-friendly places to visit")
+  print("5. Exit")
 
-#Include one menu option for exiting the conversation. This option should display a goodbye message and end your program.
-def handle_choice(choice, pet_name, pet_breed):
-    if choice== '5':
-        print("Goodbye! Have a great day with your pet!")
-    else:
-        print("Sorry, I didn't understand that choice. Please try again.")
+def handle_choice (choice, pet_name, pet_breed):
+  if choice=='5':
+    print("Goodbye! Have a great day with your pet!")
+  else:
+    print("Sorry, I didn't understand that choice. Please try again.")
+
+welcome_user()
+user_name, user_age= collect_user_info()
+pet_name, pet_type= collect_pet_info()
+display_menu()
+choice = input("Enter your choice")
+handle_choice(choice, pet_name, pet_type)
